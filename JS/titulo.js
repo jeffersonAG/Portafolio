5f08d4a47da855246a21db49c2b170811f2b316d
jeffersonAG <gutierrez22754@gmail.com>
2024-10-31 04:36:43 -0600

    nombre versión 2
    versión 2 del nombre con el titulo de de lo que voy a ejercer

--- JS/titulo.js
+++ JS/titulo.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const loadingBar = document.getElementById("loadingBar");
     const titleContainer = document.querySelector('.title-container');
     const titleText = document.querySelector('.title h1');
+    const subtitleText = document.querySelector('.subtitle'); // Subtítulo
 
     // Simular una carga
     let loadingPercentage = 0;
@@ -26,11 +27,23 @@ document.addEventListener("DOMContentLoaded", () => {
                     index++;
                     setTimeout(typeText, 100); // Controlar la velocidad de escritura
                 } else {
-                    
+                    typeSubtitle(); // Llamar a la función de subtítulo después de que termine el título
                 }
             }
 
-            // Iniciar el efecto de escritura
+            // Función para escribir el subtítulo
+            const subtitle = "Desarrollador de Software y Analista de Ciber Seguridad";
+            let subtitleIndex = 0;
+
+            function typeSubtitle() {
+                if (subtitleIndex < subtitle.length) {
+                    subtitleText.innerHTML += subtitle.charAt(subtitleIndex);
+                    subtitleIndex++;
+                    setTimeout(typeSubtitle, 100); // Controlar la velocidad de escritura
+                }
+            }
+
+            // Iniciar el efecto de escritura del título
             typeText();
         }
     }, 100); // Cambiar el intervalo de carga
